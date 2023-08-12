e23650f4c7071a3c92cb3eec1adf9c2ba81fbfd6
ly <1442404089@qq.com>
2023-08-12 23:34:21 +0800

    hot-fix commit-1

--- src/main/java/com/atly/git/GitTest.java
+++ src/main/java/com/atly/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello github!");
-        System.out.println("Hello github!");
+        System.out.println("Hello github2!");
+        System.out.println("Hello github3!");
     }
 }
